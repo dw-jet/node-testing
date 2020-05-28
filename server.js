854d96c36a837c1eff6fcbe148169224e8921658
James Teitsworth <james@jamesteitsworth.com>
2020-05-28 09:41:51 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const appData = {"id": 42, "result": "The answer"};
+// Pull in lib
+const express = require('express');
+// Make an app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser');
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize project folder
+app.use(express.static('demo'));
+
+const port = 3030;
+// Spin up a server
+const server = app.listen(port, listening);
+// Another way of doing that with an arrow function
+// const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
+
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+app.get('/', function (req, res) {
+    // res.send("Hello Mr. Teitsworth");
+})
+
+app.get('/all', (req, res) => {
+    res.send(appData);
+})
